4d67260d7729239d2fccb9071344a302eb71d36d
daisukeokamoto1023 <okadaih11023@gmail.com>
2021-04-29 20:55:27 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # crawler
 補助金の情報を収集するcrawler
+
+# Emulatorの起動方法
+firebaseをローカルで動かすためのemulatorを起動する
+https://firebase.google.com/docs/emulator-suite/install_and_configure?hl=ja
---
+++ functions/src/index.ts
@@ -0,0 +1,14 @@
+import * as functions from "firebase-functions";
+
+import * as admin from "firebase-admin"
+admin.initializeApp()
+
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+
+    const original = request.query.text
+    const writeResult = await admin.firestore().collection('messages').add({ original: original })
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.json({
+        result: `Message with ID: ${writeResult.id} added!`
+    });
+});
